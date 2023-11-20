a47d5ddf6613c6fe739db49b88e1b689de827f7f
Roberto Butti <roberto.butti@gmail.com>
2023-11-20 09:20:47 +0100

    Create teaser.blade.php

---
+++ resources/views/components/teaser.blade.php
@@ -0,0 +1,7 @@
+
+<div class="card w-96 bg-base-100 shadow-xl">
+    <div class="card-body">
+        <h2 class="card-title">Component {{ $component["component"] }}</h2>
+        <p>{{ $component["headline"] }}</p>
+    </div>
+</div>
