efc35b7f605346ffd2084f122923754af73329f4
Roberto Butti <roberto.butti@gmail.com>
2023-11-27 15:26:38 +0100

    better style for teaser

--- resources/views/components/page.blade.php
+++ resources/views/components/page.blade.php
@@ -1,4 +1,4 @@
-<main {!! $editableAttributes !!}>
+<main {!! $editableAttributes !!} class="w-full place-items-center">
 
     @foreach($component['body'] as $key => $value)
         <x-storyblok-component :component="$value" />
--- resources/views/components/teaser.blade.php
+++ resources/views/components/teaser.blade.php
@@ -1,8 +1,15 @@
 
-<div {!! $editableAttributes !!} class="card w-full bg-base-100 shadow-xl">
+<div {!! $editableAttributes !!} class="w-full">
+    <div class="text-center bg-white bg-opacity-70 px-2 pt-40 lg:px-10">
+        <h2 class="font-title relative z-[2] mx-auto text-[clamp(2rem,6vw,4.5rem)] font-black leading-none">
+            <span style="opacity:1">{{ $component["headline"] }}</span> <br />
 
-    <div class="card-body">
-        <h2 class="card-title">Component {{ $component["component"] }}</h2>
-        <p>{{ $component["headline"] }}</p>
+        </h2>
+        <p class="text-base-content/90 font-title relative z-[2] py-4 font-light md:text-3xl">
+            {{ Arr::get($component, "subheadline", "") }}
+        </p>
+        <div class="h-10" />
+
     </div>
+
 </div>
